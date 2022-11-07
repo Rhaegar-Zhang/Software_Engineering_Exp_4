d5999f60fb573b34444860e0406ba623ed826533
Zhang <zys1992088590@gmail.com>
2022-11-07 22:59:09 +0800

    Head file of input build finished

--- input.h
+++ input.h
@@ -1,26 +1,42 @@
-class IntInput
-{
-public:
+#include <vector>
+#include <fstream>
+#include <string>
 
+//Base class of all possible inputs.
+class Input{
+public:
+    virtual void gen_input() = 0;
+};
+
+//Class represents input with type of integer
+class IntInput : public Input {
+public:
+    void gen_input();
 private:
     int min_value, max_value;
-protected:
 };
 
-
-class StringInput
-{
+//Class represents input with type of string.
+class StringInput : public Input {
 public:
+    void gen_input();
+private:
     int min_length, max_length;
-private:
-
-protected:
 };
 
-class CharInput : public StringInput
-{
+//Class represents input with type of char.
+class CharInput : public StringInput {
 public:
-private:
+    void gen_input();
+};
 
-protected:
+//Class to generate random inputs.
+class InputGen{
+private:
+    //Store the input generated randomly.
+    std::vector<std::vector<Input*> > program_input;
+    //Read the stdin.txt file.
+    void read_stdin_file();
+public:
+    void gen_input(int num, std::string stdin_path);
 };
