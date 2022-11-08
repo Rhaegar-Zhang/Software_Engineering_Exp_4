559930cd7f62d327637feda25d062fca697c04a1
Zhang <zys1992088590@gmail.com>
2022-11-08 14:25:55 +0800

    commit1

--- input.cpp
+++ input.cpp
@@ -1,2 +1,47 @@
 #include "input.h"
 
+IntInput::IntInput(int min_value, int max_value){
+    this->min_value = min_value;
+    this->max_value = max_value;
+}
+
+void IntInput::gen_input(){
+    random_input = (rand() % (max_value - min_value + 1)) + min_value;
+}
+
+int IntInput::get_random_input(){
+    return random_input;
+}
+
+StringInput::StringInput(int min_len, int max_len){
+    this->min_length = min_len;
+    this->max_length = max_len;
+    random_input = NULL;
+}
+
+StringInput::~StringInput(){
+    delete []random_input;
+    random_input = NULL;
+}
+
+void StringInput::gen_input(){
+    int random_len = (rand() % (max_length - min_length + 1)) + min_length;
+    random_input = new char[random_len];
+    for(int i = 0; i < random_len; ++i)
+        random_input[i] = rand() % 2 ? 'a' + rand() % 26 : 'A' + rand() % 26;
+}
+
+char* StringInput::get_random_input(){
+    return random_input;
+}
+
+void InputGen::read_stdin_file(std::string stdin_path){
+    std::ifstream fin(stdin_path);
+    if(!fin)
+        return;
+    while(!fin.eof()){
+        std::string s;
+        if(!getline(fin, s)) break;
+        
+    }
+}
--- input.h
+++ input.h
@@ -1,33 +1,42 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 
 //Base class of all possible inputs.
 class Input{
 public:
     virtual void gen_input() = 0;
+    virtual ~Input() {}
 };
 
 //Class represents input with type of integer
 class IntInput : public Input {
 public:
+    IntInput(int, int);
     void gen_input();
+    int get_random_input();
 private:
     int min_value, max_value;
+    int random_input;
 };
 
 //Class represents input with type of string.
 class StringInput : public Input {
 public:
+    StringInput(int, int);
+    ~StringInput();
     void gen_input();
+    char* get_random_input();
 private:
     int min_length, max_length;
+    char* random_input;
 };
 
 //Class represents input with type of char.
 class CharInput : public StringInput {
 public:
-    void gen_input();
+    CharInput() : StringInput(1, 1) {};
 };
 
 //Class to generate random inputs.
@@ -36,7 +45,7 @@ private:
     //Store the input generated randomly.
     std::vector<std::vector<Input*> > program_input;
     //Read the stdin.txt file.
-    void read_stdin_file();
+    void read_stdin_file(std::string);
 public:
     void gen_input(int num, std::string stdin_path);
 };
