8f9167e25d66c4783ad66d865d77fe4b96527ccc
Zhang <zys1992088590@gmail.com>
2022-11-11 00:03:43 +0800

    finished

---
+++ get_file_tree.cpp
@@ -0,0 +1,59 @@
+#include "get_file_tree.h"
+
+GetFileTree::GetFileTree(){
+    file_tree = new std::vector<FileTreeBranch*>();
+}
+
+GetFileTree::~GetFileTree(){
+    delete file_tree;
+}
+
+FileTreeBranch* GetFileTree::gen_singal_branch(char* basepath){
+    DIR *dir;
+    struct dirent *ptr;
+    FileTreeBranch* p = new FileTreeBranch;
+    int i = 0;
+    if((dir = opendir(basepath)) == NULL)
+        exit(1);
+    while((ptr = readdir(dir)) != NULL){
+        if(strcmp(ptr->d_name, ".") == 0 || strcmp(ptr->d_name, "..") == 0)
+            continue;
+        else if(ptr->d_type == 8){
+            std::string abs_path = std::string(basepath) + std::string(ptr->d_name);
+            p->singal_branch.push_back(abs_path);
+            if(abs_path.find(".txt") != std::string::npos){
+                p->stdin_index = i;
+            }
+            i++;
+        }
+    }
+    return p;
+}
+
+void GetFileTree::gen_file_tree(char* basepath){
+    DIR *dir;
+    struct dirent *ptr;
+    char base[1000];
+    if((dir = opendir(basepath)) == NULL)
+        exit(1);
+    while((ptr = readdir(dir)) != NULL){
+        if(strcmp(ptr->d_name, ".") == 0 || strcmp(ptr->d_name, "..") == 0)
+            continue;
+        else if(ptr->d_type == 8){
+            FileTreeBranch* p = gen_singal_branch(basepath);
+            file_tree->push_back(p);
+            return;
+        }
+        else if(ptr->d_type == 4){
+            memset(base, '\0', sizeof(base));
+            strcpy(base, basepath);
+            strcat(base, "/");
+            strcat(base, ptr->d_name);
+            gen_file_tree(base);
+        }
+    }
+}
+
+std::vector<FileTreeBranch*>* GetFileTree::get_file_tree(){
+    return this->file_tree;
+}
---
+++ get_file_tree.h
@@ -0,0 +1,29 @@
+#ifndef GET_FILE_TREE
+#define GET_FILE_TREE
+
+#include <cstdio>
+#include <unistd.h>
+#include <dirent.h>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <string>
+
+class FileTreeBranch{
+public:
+    int stdin_index;
+    std::vector<std::string> singal_branch;
+};
+
+class GetFileTree{
+private:
+    std::vector<FileTreeBranch*>* file_tree;
+public:
+    GetFileTree();
+    ~GetFileTree();
+    void gen_file_tree(char* basepath);
+    FileTreeBranch* gen_singal_branch(char* basepath);
+    std::vector<FileTreeBranch*>* get_file_tree();
+};
+
+#endif
