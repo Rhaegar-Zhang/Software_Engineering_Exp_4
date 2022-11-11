224ef9b172ec7c6dd6934f4d1704f238c10ddf31
Zhang <zys1992088590@gmail.com>
2022-11-11 15:42:50 +0800

    update

--- execute.h
+++ execute.h
@@ -5,10 +5,11 @@
 #include <unistd.h>
 #include <string>
 #include <dirent.h>
+#include <cstring>
 
 class Execute{
 public:
-    void exec_program();
+    void exec_program(std::string par_path, std::string file_name, int test_num);
 };
 
 #endif
