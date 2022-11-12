c21094533f2c6671d5bb798c5cca4a9f476e211c
Zhang <zys1992088590@gmail.com>
2022-11-12 11:42:01 +0800

    update

--- get_file_tree.cpp
+++ get_file_tree.cpp
@@ -15,7 +15,7 @@ FileTreeBranch* GetFileTree::gen_singal_branch(char* basepath){
     if((dir = opendir(basepath)) == NULL)
         exit(1);
     int i = 0;
-    p->curr_dir = std::string(basepath);
+    p->curr_dir = std::string(basepath) + "/";
     while((ptr = readdir(dir)) != NULL){
         if(strcmp(ptr->d_name, ".") == 0 || strcmp(ptr->d_name, "..") == 0)
             continue;
